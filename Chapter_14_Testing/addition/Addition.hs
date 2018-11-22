31cd86b39cc95b9e09c6ca480e72e947a5f00386
Júlia Garrigós <julia.garrigos@xing.com>
2018-11-22 11:28:33 +0100

    Indentation fix

--- Chapter_14_Testing/addition/Addition.hs
+++ Chapter_14_Testing/addition/Addition.hs
@@ -10,8 +10,8 @@ main = hspec $ do
 
 main' :: IO ()
 main' = hspec $ do
-describe "Addition" $ do
-  it "1 + 1 is greater than 1" $ do
-    (1 + 1) > 1 `shouldBe` True
-  it "2 + 2 is equal to 4" $ do
-    2 + 2 `shouldBe` 4
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
